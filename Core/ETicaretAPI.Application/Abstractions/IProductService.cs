857f940ef0dd71734c5c4a48ae8df8ade84b4cfb
Mustafa Kurt <mustafa.kurt.0@icloud.com>
2022-09-07 06:14:39 +0300

    Add project files.

---
+++ Core/ETicaretAPI.Application/Abstractions/IProductService.cs
@@ -0,0 +1,8 @@
+﻿using ETicaretAPI.Domain.Entities;
+
+namespace ETicaretAPI.Application.Abstractions;
+
+public interface IProductService
+{
+    List<Product> GetProducts();
+}
---
+++ Infrastructure/ETicaretAPI.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,27 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Domain.Entities;
+
+namespace ETicaretAPI.Persistence.Concretes;
+
+public class ProductService : IProductService
+{
+    public List<Product> GetProducts() => new()
+    {
+        new()
+        {
+            Id = Guid.NewGuid(),
+            CreatedDate = DateTime.Now,
+            Name = "Product",
+            Price = 100,
+            Stock = 10
+        },
+        new()
+        {
+            Id = Guid.NewGuid(),
+            CreatedDate = DateTime.Now,
+            Name = "Product 2",
+            Price = 200,
+            Stock = 8
+        }
+    };
+}
